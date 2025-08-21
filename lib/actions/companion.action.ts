3b12b2e7c93c8ad4c414e3e79ff02f35026cdb7e
Abuzarzubair <abuzarzubair15@gmail.com>
2025-08-21 06:41:53 -0700

    fixed companion.action.ts

--- lib/actions/companion.action.ts
+++ lib/actions/companion.action.ts
@@ -2,7 +2,7 @@
 import { auth } from "@clerk/nextjs/server";
 import { createSupabaseClient } from "../supabase";
 
-export const createCompanion = async (formData: any) => {
+export const createCompanion = async (formData: CreateCompanion) => {
   const { userId: author } = await auth();
   const supabase = createSupabaseClient();
 
