95d8798ccf4f23233656eaceb86c9094755d55af
Abuzarzubair <abuzarzubair15@gmail.com>
2025-08-22 04:47:39 -0700

    fixed error

--- lib/utils.ts
+++ lib/utils.ts
@@ -12,14 +12,15 @@ export const getSubjectColor = (subject: string) => {
 };
 
 export const configureAssistant = (voice: string, style: string) => {
-  const voiceId = voices[voice as keyof typeof voices][
-          style as keyof (typeof voices)[keyof typeof voices]
-          ] || "sarah";
+  const voiceId =
+    voices[voice as keyof typeof voices][
+      style as keyof (typeof voices)[keyof typeof voices]
+    ] || "sarah";
 
   const vapiAssistant: CreateAssistantDTO = {
     name: "Companion",
     firstMessage:
-        "Hello, let's start the session. Today we'll be talking about {{topic}}.",
+      "Hello, let's start the session. Today we'll be talking about {{topic}}.",
     transcriber: {
       provider: "deepgram",
       model: "nova-3",
@@ -54,8 +55,8 @@ export const configureAssistant = (voice: string, style: string) => {
         },
       ],
     },
-    clientMessages: [],
-    serverMessages: [],
+    clientMessages: [] as unknown as CreateAssistantDTO["clientMessages"],
+    serverMessages: [] as unknown as CreateAssistantDTO["serverMessages"], 
   };
   return vapiAssistant;
 };
