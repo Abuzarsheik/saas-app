9618d81d4503f8e6f76945052442eb27d397c10d
Abuzarzubair <abuzarzubair15@gmail.com>
2025-08-22 09:15:51 -0700

    implemented subscription lock

--- app/companion/new/page.tsx
+++ app/companion/new/page.tsx
@@ -1,16 +1,44 @@
 import CompanionForm from "@/components/companionform";
+import { newCompanionPermissions } from "@/lib/actions/companion.action";
 import { auth } from "@clerk/nextjs/server";
+import Image from "next/image";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function NewCompanion() {
   const { userId } = await auth();
   if (!userId) redirect("/sign-in");
+  const canCreateCompanion = await newCompanionPermissions();
   return (
     <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
-      <article className="w-full gap-4 flex flex-col">
-        <h1>Companion Builder</h1>
-        <CompanionForm />
-      </article>
+      {canCreateCompanion ? (
+        <article className="w-full gap-4 flex flex-col">
+          <h1>Companion Builder</h1>
+
+          <CompanionForm />
+        </article>
+      ) : (
+        <article className="companion-limit">
+          <Image
+            src="/images/limit.svg"
+            alt="Companion limit reached"
+            width={360}
+            height={230}
+          />
+          <div className="cta-badge">Upgrade your plan</div>
+          <h1>You have Reached your limit</h1>
+          <p>
+            You have reached your companion limit. upgrade to create more
+            companions and access premium features
+          </p>
+          <Link
+            href="/subscription"
+            className="btn-primary w-full justify-center"
+          >
+            Upgrade my plan
+          </Link>
+        </article>
+      )}
     </main>
   );
 }
